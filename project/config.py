36ce5be65362b43905884cf4991afc53f12e6930
GamerBoi801 <geeseplayer@gmail.com>
2025-01-23 08:56:18 +0500

    gitignore is now working

--- project/config.py
+++ project/config.py
@@ -1,11 +1,7 @@
 # config.py
 
-# Database path for storing passwords
+# database path for storing passwords
 DB_PATH = 'password_manager.db'
 
 # Default password length for generated passwords
 DEFAULT_PASSWORD_LENGTH = 16
-
-# Encryption settings
-KEY_SIZE = 16  # Size in bytes for AES-128 (16 bytes = 128 bits)
-BLOCK_SIZE = 16  # Block size for AES (always 16 bytes)
